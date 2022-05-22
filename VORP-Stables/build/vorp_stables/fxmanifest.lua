3926fdd90b701ad5c48b2df817b724a577c8ca56
local9 <apjcookie1@hotmail.com>
2022-05-22 10:45:30 +0100

    updated fxmanifest globbing

--- VORP-Stables/build/vorp_stables/fxmanifest.lua
+++ VORP-Stables/build/vorp_stables/fxmanifest.lua
@@ -3,11 +3,11 @@ fx_version 'adamant'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 client_scripts {
-  'VORP.Stables.Client.net.dll'
+  '*.Client.net.dll'
 }
 
 server_scripts {
-  'VORP.Stables.Server.net.dll'
+  '*.Server.net.dll'
 }
 
 files {
